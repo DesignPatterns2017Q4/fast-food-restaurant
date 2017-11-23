92750b0987db053cc8fc5799d712140afe6217c3
Andras Szoke <v-aszoke@expedia.com>
2017-11-23 15:29:16 +0100

    Add placeholder Client class and unit test, add SureFire plugin to pom.xml to run unit tests

---
+++ src/main/java/com/epam/training/designpatterns/fastfoodrestaurant/Client.java
@@ -0,0 +1,5 @@
+package com.epam.training.designpatterns.fastfoodrestaurant;
+
+public class Client {
+
+}
---
+++ src/test/java/com/epam/training/designpatterns/fastfoodrestaurant/ClientTest.java
@@ -0,0 +1,12 @@
+package com.epam.training.designpatterns.fastfoodrestaurant;
+
+import org.junit.Test;
+
+public class ClientTest {
+
+	@Test
+	public void test() {
+		// Not yet implemented
+	}
+
+}
