9f28284b456787f6650d7acbd678ad7656c48638
Robert Toth <Robert_Toth@epam.com>
2017-12-03 21:49:27 +0100

    Added more orders

--- src/main/java/com/epam/training/designpatterns/fastfoodrestaurant/Main.java
+++ src/main/java/com/epam/training/designpatterns/fastfoodrestaurant/Main.java
@@ -6,13 +6,17 @@ import java.util.Queue;
 public class Main {
     public static void main(String[] args) {
         Queue<Order> orders = new LinkedList<Order>();
-        
         Chef chef = new Chef(orders);
         Waiter waiter = new Waiter(chef, orders);
-        Client client = new Client(waiter, Menu.FoodType.Hotdog, Menu.ExtraType.Ketchup);
         
-        Order order = client.makeOrder();
-        waiter.addOrder(order);
+        Client client1 = new Client(waiter, Menu.FoodType.Hotdog, Menu.ExtraType.Ketchup);
+        Client client2 = new Client(waiter, Menu.FoodType.Chips, Menu.ExtraType.Mustard);
+        
+        Order order1 = client1.makeOrder();
+        Order order2 = client2.makeOrder();
+        waiter.addOrder(order1);
+        waiter.addOrder(order2);
+        chef.makeFood();
         chef.makeFood();
     }    
 }
