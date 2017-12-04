f452171d5fc857c3d1c97b629dc79c6ef341cb84
Robert Toth <Robert_Toth@epam.com>
2017-12-04 20:39:50 +0100

    Add dumy test

--- src/test/java/com/epam/training/designpatterns/fastfoodrestaurant/ClientTest.java
+++ src/test/java/com/epam/training/designpatterns/fastfoodrestaurant/ClientTest.java
@@ -1,17 +1,24 @@
 package com.epam.training.designpatterns.fastfoodrestaurant;
 
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.Queue;
+import java.util.LinkedList;
 
 public class ClientTest {
 
 	@Test
 	public void testCreateNewClient() {
 
-		System.out.print("Creating new client...");
+		Queue<Order> orders = new LinkedList<Order>();
+        Chef chef = new Chef(orders);
+        Waiter waiter = new Waiter(chef, orders);
+        
+        Client client = new Client(waiter, Menu.FoodType.Hotdog, Menu.ExtraType.Ketchup);
+        
+		Assert.assertEquals(Client.class, client.getClass());
 
-		//Client client = new Client();
-
-		//System.out.println("DONE: " + client);
 	}
 
 }
