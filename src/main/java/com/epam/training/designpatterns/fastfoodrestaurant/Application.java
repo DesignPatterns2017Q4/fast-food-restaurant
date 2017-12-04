f61b331c0d1353e3ebc31a0fb4e80fd4c3d347f3
Andras Szoke <v-aszoke@expedia.com>
2017-12-04 11:53:46 +0100

    Create Application class, update POM to build a runnable JAR

---
+++ src/main/java/com/epam/training/designpatterns/fastfoodrestaurant/Application.java
@@ -0,0 +1,11 @@
+package com.epam.training.designpatterns.fastfoodrestaurant;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+        Client client = new Client();
+
+        System.out.println("Client created: " + client);
+    }
+}
